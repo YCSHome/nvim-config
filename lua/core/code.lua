72883021d0eb7e5bad3bf53b1363a7b1b4680d07
liuyi <3113929638@qq.com>
2022-02-27 08:00:47 +0800

    2022.2.27

--- lua/core/code.lua
+++ lua/core/code.lua
@@ -1,25 +1,90 @@
-vim.api.nvim_set_keymap('n', '<leader>b', ':lua RunCode()<CR>', { noremap = true, silent = true })
+--vim.api.nvim_set_keymap('n', '<leader>b', ':lua RunCode()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ft', ':FloatermNew<CR>', { noremap = true, silent = true })
 
 local BuildOption = {
-  ["mode"] = 1,
   ["focus"] = 0,
   ["save"] = 2,
   ["mode"] = "term",
   ["rows"] = 10
 }
 
-function AsyncRun(opt, cmd)
+local AsyncRun = function(opt, cmd)
   vim.call("asyncrun#run", "!", opt, cmd)
 end
 
-function RunCode()
-  FileType = vim.opt.filetype["_value"]
-  if (FileType == 'cpp') then
-    AsyncRun(BuildOption, "clang++ $(VIM_FILENAME) && echo build && ./a.out<~/.input && echo done")
-  elseif (FileType == 'lua') then
-    AsyncRun(BuildOption, "lua $(VIM_FILENAME)")
-  elseif (FileType == 'python') then
-    AsyncRun(BuildOption, "python3 $(VIM_FILENAME)")
+function table.merge(table1, table2)
+  for key, value in pairs(table2) do
+    table1[key] = value
   end
+  return table1
 end
+
+Code = {
+  RunByCodeTypeList = function(CodeTypeList)
+    FileType = vim.opt.filetype["_value"]
+
+    for _, CodeType in pairs(CodeTypeList) do
+      if (CodeType.check() == true) then
+        CodeType.run()
+        return 0
+      end
+    end
+    print("not such code type "..tostring(#CodeTypeList))
+  end,
+
+  BaseCodeType = {
+    check = function()
+      return false
+    end,
+    run = function()
+    end
+  }
+}
+
+CodeTypeList = { }
+
+function AddCodeTypeList(Key, List)
+  table.insert(CodeTypeList, List)
+  vim.api.nvim_set_keymap("n", Key, ":lua Code.RunByCodeTypeList(CodeTypeList["..tostring(#CodeTypeList).."])<CR>", {noremap = true, silent = true})
+end
+
+Code.Cpp = {
+  check = function()
+    if (FileType == 'cpp') then
+      return true
+    end
+  end,
+  run = function()
+    AsyncRun(BuildOption, "clang++ $(VIM_FILENAME) && echo build && ./a.out<~/.input && echo done")
+  end
+}
+
+Code.Lua = {
+  check = function()
+    if (FileType == 'lua') then
+      return true
+    end
+  end,
+
+  run = function()
+    AsyncRun(BuildOption, "lua $(VIM_FILENAME)")
+  end
+}
+
+table.merge(CodeTypeList, Code)
+
+AddCodeTypeList("<leader>b", {
+    Code.Cpp,
+    Code.Lua,
+    {
+      check = function()
+        if (FileType == 'python') then
+          return true
+        end
+      end,
+      run = function()
+        AsyncRun(BuildOption, "python3 $(VIM_FILENAME)")
+      end
+    }
+  }
+  )
